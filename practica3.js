eb709f42b88d0de196ac5380b06bd5fb6a584afd
Eliana Aliz <elianaaliz16@gmail.com>
2019-04-09 15:42:25 +0100

    Enemigo Goomba
    Agregado al enemigo Goomba, y su funcion de matar a mario y cuando el muere si mario salta sobre él

--- practica3.js
+++ practica3.js
@@ -5,6 +5,48 @@ window.addEventListener("load", function () {
     Q.setup({ maximize: false, width: 320, height: 480 });
     Q.controls();
     Q.touch();
+
+    var gameOver = false;   //?
+     /**------------------SPRITE GOOMBA----------------- */
+    Q.component("Enemy", {
+		added: function(){
+			this.entity.on("bump.top",function(collision) { //muere Enemy
+	      		if(collision.obj.isA("Mario")) { 
+                      this.p.vx = 0;
+                      this.destroy();   
+	        		collision.obj.p.vy = -200;
+	      		}
+	    	});
+
+	    	this.entity.on("bump.left,bump.right",function(collision) { //muere Mario
+	    		if(collision.obj.isA("Mario")) { 
+	        		collision.obj.dying();
+	      		}
+	    	});
+
+		}
+    });
+    
+    Q.Sprite.extend("Goomba",{
+        init: function (p) {
+
+            this._super(p, {
+                sprite: "goomba", sheet: "goomba",
+                vx: 30,
+            });
+            this.add('2d, aiBounce, Enemy');
+        },
+
+        step:function(dt){
+            if(!gameOver){
+                //algo
+            } 
+            else{
+                //algo mas
+                this.del('2d');
+            } 
+        }
+    });
     /**------------------SPRITE MARIO BROS----------------- */
     Q.Sprite.extend("Mario", {
         init: function (p) {
@@ -15,13 +57,13 @@ window.addEventListener("load", function () {
             });
             this.add('2d, platformerControls');
 
-            this.on("died", this, "restart");
         },
 
         dying: function () {
-            this.del('2d, platformerControls'); //elimina
+            //this.del('2d, platformerControls'); //elimina
+            //gameOver = true;
             Q.stageScene("level1"); //volvemos al inicio
-            //booleano End?
+           
         },
 
         step: function (dt) {
@@ -36,14 +78,17 @@ window.addEventListener("load", function () {
     Q.scene("level1", function (stage) {
         Q.stageTMX("level.tmx", stage);  //PUESTA EN ESCENA
 
+        stage.insert(new Q.Goomba({x: 280, y: 528}));   //Insertamos al enemigo Goomba
         var mario = stage.insert(new Q.Mario());    //Mario a escena
         stage.add("viewport").follow(mario, { x: true, y: false }); //Seguir a Mario
         stage.centerOn(150, 380);
     });
 
-    Q.load("mario_small.png, mario_small.json", function () {   //Cargamos los sprites
+    Q.load("mario_small.png, mario_small.json, goomba.png, goomba.json", function () {   //Cargamos los sprites
+        Q.sheet("goomba","goomba.png", { tilew: 28, tileh: 28 });
         Q.sheet("mario_small", "mario_small.png", { tilew: 32, tileh: 32 });
         Q.compileSheets("mario_small.png", "mario_small.json");
+        Q.compileSheets("goomba.png","goomba.json");
 
     });
 
